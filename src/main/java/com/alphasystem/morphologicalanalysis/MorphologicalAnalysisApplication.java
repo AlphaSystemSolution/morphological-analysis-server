53b3e0101ea4e50371c45933ad1c93dd404cbe76
sfali23 <f.syed.ali@gmail.com>
2017-02-03 13:37:26 -0500

    initial commit

---
+++ src/main/java/com/alphasystem/morphologicalanalysis/MorphologicalAnalysisApplication.java
@@ -0,0 +1,15 @@
+package com.alphasystem.morphologicalanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sali
+ */
+@SpringBootApplication
+public class MorphologicalAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorphologicalAnalysisApplication.class, args);
+    }
+}
---
+++ src/main/java/com/alphasystem/morphologicalanalysis/spring/support/DatabaseConfiguration.java
@@ -0,0 +1,50 @@
+package com.alphasystem.morphologicalanalysis.spring.support;
+
+import com.mongodb.Mongo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+/**
+ * @author sali
+ */
+@Configuration
+@EnableMongoRepositories(basePackages = {"com.alphasystem.morphologicalanalysis.wordbyword.repository",
+        "com.alphasystem.morphologicalanalysis.graph.repository",
+        "com.alphasystem.morphologicalanalysis.morphology.repository"})
+@Import(value = MongoAutoConfiguration.class)
+public class DatabaseConfiguration extends AbstractMongoConfiguration {
+
+    @Autowired
+    private Mongo mongo;
+
+    @Autowired
+    private MongoProperties mongoProperties;
+
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return mongoProperties.getDatabase();
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return mongo;
+    }
+}
