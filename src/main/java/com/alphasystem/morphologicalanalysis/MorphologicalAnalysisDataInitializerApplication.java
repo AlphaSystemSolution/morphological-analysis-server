cdd09babad89450d6dccb0c6c886e8d984da67a0
sfali23 <f.syed.ali@gmail.com>
2017-02-07 16:57:09 -0500

    Spring boot application to load data

---
+++ src/main/java/com/alphasystem/morphologicalanalysis/MorphologicalAnalysisDataInitializerApplication.java
@@ -0,0 +1,29 @@
+package com.alphasystem.morphologicalanalysis;
+
+import com.alphasystem.morphologicalanalysis.util.DataInitializationTool;
+import com.alphasystem.morphologicalanalysis.util.Script;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sali
+ */
+@SpringBootApplication
+public class MorphologicalAnalysisDataInitializerApplication implements CommandLineRunner {
+
+    @Autowired private DataInitializationTool dataInitializationTool;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        for (Script script : Script.values()) {
+            dataInitializationTool.createAllChapters(script);
+        }
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorphologicalAnalysisDataInitializerApplication.class, args);
+    }
+}
